14e157d545dd53f442a1337d24036097af577699
littlechai <767733103@qq.com>
2021-02-09 12:36:58 +0800

    '2021.02.09'

--- miniprogram/pages/editDiary/editDiary.js
+++ miniprogram/pages/editDiary/editDiary.js
@@ -46,7 +46,20 @@ Page({
     uploadTitle: "",
     uploadBol: false,
     trueData: {},
-    showCal: false
+    showCal: false,
+    getBol: false,
+
+    chatsBol: false,
+    chatsIndex: 0,
+    chatsName: '',
+    chatsOpenid: '',
+    loginBol: false,
+    loginTitle: '',
+    loginDuration: 0,
+    loginIcon: '',
+    commentArr: [],
+    commentNum: 0,
+    commentBol: false
   },
   // 初始化自定义导航栏
   async firstHeader() {
@@ -82,7 +95,8 @@ Page({
     this.setData({
       trueData: result,
       info: copyInfo,
-      diaryArr: copyArr
+      diaryArr: copyArr,
+      getBol: true
     })
     // this.sortFn({
     //   detail: {
@@ -608,6 +622,180 @@ Page({
 
   },
 
+
+  // 评论事件
+  // 评论头像点击
+  toUsers(e) {
+    wx.navigateTo({
+      url: '../userInfo/userInfo?id=' + e.currentTarget.dataset.openid,
+    })
+  },
+  // 评论内容
+  getChat(e) {
+    this.setData({
+      chatData: e.detail.value
+    })
+  },
+  //回复评论
+  async uploadChat() {
+    let that = this;
+    if (!wx.getStorageSync('openid')) {
+      this.setData({
+        loginBol: true,
+        loginTitle: '请先登录',
+        loginDuration: 1500
+      })
+      return;
+    }
+
+    if (this.data.chatData == '') {
+      this.setData({
+        loginBol: true,
+        loginTitle: '请输入内容',
+        loginDuration: 2000
+      })
+    } 
+    else {
+      this.setData({
+        loginBol: true,
+        loginIcon: 'loading',
+        loginTitle: '评论中',
+        loginDuration: 99999
+      })
+  
+      //楼中楼
+      if(this.data.chatsBol) {  
+        let time = await app.timeStampX(new Date().getTime());
+
+        let res = await wx.cloud.callFunction({
+          name: 'uploadComment',
+          data: {
+            diary_id: this.data.id,
+            updatedTime: time,
+            openid: wx.getStorageSync('openid'),
+            comment_openid: this.data.chatsOpenid,
+            comment_index: this.data.chatsIndex,
+            bol: true,
+            content: this.data.chatData
+          }
+        })
+        console.log(res)
+        if(res.result.errMsg === "collection.update:ok") {
+          this.setData({
+            loginBol: true,
+            loginIcon: 'success',
+            loginTitle: '评论成功',
+            loginDuration: 1500,
+            chatData: '',
+            chatsName: '',
+            chatsOpenid: '',
+            pText: '留下你的评论呀~',
+            chatsBol: false
+          })
+
+          this.getComment();
+        }
+
+      }
+      //正常评论
+      else {
+        let time = await app.timeStampX(new Date().getTime());
+
+        let res = await wx.cloud.callFunction({
+          name: 'uploadComment',
+          data: {
+            diary_id: this.data.id,
+            updatedTime: time,
+            createdTime: time,
+            openid: wx.getStorageSync('openid'),
+            bol: false,
+            content: this.data.chatData
+          }
+        })
+
+        if(res.result.errMsg === "collection.update:ok") {
+          this.setData({
+            loginBol: true,
+            loginIcon: 'success',
+            loginTitle: '评论成功',
+            loginDuration: 1500,
+            chatData: ""
+          })
+
+          this.getComment();
+        }
+         
+      }
+    }
+
+  },
+
+  //获得评论内容
+  async getComment() {
+    console.log(this.data.id)
+    let res = await wx.cloud.callFunction({
+      name: 'getComment',
+      data: {
+        diary_id: this.data.id
+      }
+    })
+    console.log(res)
+    let num = 0;
+    for(let i=0; i<res.result.length; i++) {
+      num++;
+      for(let j=0; j<res.result[i].arr.length; j++) {
+        num++
+      }
+    }
+    this.setData({
+      commentArr: res.result,
+      commentNum: num
+    })
+  },
+
+  //楼中楼
+  chattochat(e) {
+    let index = e.currentTarget.dataset.index;
+    let name = e.currentTarget.dataset.name;
+    this.setData({
+      chatsIndex: index,
+      chatsName: name,
+      chatData: '',
+      pText: '回复：'+ name,
+      chatsBol: true,
+      chatsOpenid: e.currentTarget.dataset.openid
+    })
+  },
+
+  //取消楼中楼
+  cancelChats() {
+    this.setData({
+      chatData: '',
+      chatsName: '',
+      chatsOpenid: '',
+      pText: '留下你的评论呀~',
+      chatsBol: false
+    })
+  },
+
+  //获得评论权限
+  async getCommentBol() {
+    let res = await wx.cloud.callFunction({
+      name: 'getCommentBol'
+    })
+    console.log(res)
+    if(res.result.errMsg === "collection.get:ok") {
+      this.setData({
+        commentBol: res.result.data[0].controlChat,
+        refreshBol: false
+      })
+
+      if(res.result.data[0].controlChat === true) {
+        this.getComment();
+      }
+    }
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -615,8 +803,13 @@ Page({
     this.setData({
       id: options.id
     })
+    this.init();
+  },
+
+  async init() {
     this.getGlobalData();
-    this.getData();
+    await this.getData();
+    await this.getCommentBol();
   },
 
   /**
